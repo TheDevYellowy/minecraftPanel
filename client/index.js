1e77bc6d6beb9f9046807a91608f1f6b1c90e30d
TheDevYellowy <yellowycorn6963@gmail.com>
2024-01-28 15:06:22 -0600

    feat: add eula accepting

--- client/index.js
+++ client/index.js
@@ -36,69 +36,92 @@ function createWindow() {
 }
 
 async function connect(ip, password) {
-  if (socket !== null) return socket.send(JSON.stringify({ command: "getData" }));
-  socket = new WebSocket(`ws://${ip}:25500`);
+  try {
+    if (socket !== null) return socket.send(JSON.stringify({ command: "auth", pswd: password }));
+    socket = new WebSocket(`ws://${ip}:25500`);
 
-  socket.on("open", () => {
-    socket.send(JSON.stringify({ command: "auth", pswd: password }));
-  });
+    socket.on("open", () => {
+      socket.send(JSON.stringify({ command: "auth", pswd: password }));
+    });
 
-  socket.on("message", (data) => {
-    const url = getURL();
-    try {
-      const json = JSON.parse(data.toString());
-      const event = json.event;
-      delete json.event;
+    socket.on("message", (data) => {
+      const url = getURL();
+      try {
+        const json = JSON.parse(data.toString());
+        const event = json.event;
+        delete json.event;
 
-      switch (event) {
-        case "auth":
-          console.log(json);
-          if (json.success) {
-            main.loadFile("views/index.html");
-          } else {
-            main.webContents.send("wrong");
-          }
-          break;
-        case "data":
-          if (json.version) version = json.version;
-          main.webContents.send(event, json);
-          break;
+        switch (event) {
+          case "eula":
+            main.loadFile("views/eula.html");
+            break;
+          case "auth":
+            console.log(json);
+            if (json.success) {
+              main.loadFile("views/index.html");
+            } else {
+              main.webContents.send("wrong");
+            }
+            break;
+          case "data":
+            if (json.version) version = json.version;
+            main.webContents.send(event, json);
+            break;
 
-        case "log":
-          if (url !== "logs") return;
-          main.webContents.send(event, json.message);
-          break;
+          case "log":
+            if (url !== "logs") return;
+            main.webContents.send(event, json.message);
+            break;
 
-        case "logs":
-          if (url !== "logs") return;
-          main.webContents.send("logs", json.messages);
-          break;
+          case "logs":
+            if (url !== "logs") return;
+            main.webContents.send("logs", json.messages);
+            break;
 
-        case "settings":
-          if (url !== "settings") return;
-          main.webContents.send("settings", json.properties);
-          break;
+          case "settings":
+            if (url !== "settings") return;
+            main.webContents.send("settings", json.properties);
+            break;
+        }
+      } catch (error) {
+        console.error(`Error in message event: ${error}`);
       }
-    } catch (error) {
-      console.error(`Error in message event: ${error}`);
-    }
-  });
+    });
+
+    socket.once("close", () => main.close());
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 ipcMain
   .on("login", (ev, { ip, password }) => connect(ip, password))
+  .on("eula", () => socket.send(JSON.stringify({ command: "eula" })))
   .on("changeTab", (_ev, file) => main.loadFile(`views/${file}.html`))
   .on("getData", () => socket.send(JSON.stringify({ command: "getData" })))
   .on("getLogs", () => socket.send(JSON.stringify({ command: "getLogs" })))
   .on("getSettings", () => socket.send(JSON.stringify({ command: "getSettings" })))
   .on("setSettings", (_, data) => socket.send(JSON.stringify({ command: "setSettings", data })))
   .on("command", (_, cmd) => socket.send(JSON.stringify({ command: "command", message: cmd })))
-  .on("downloadPack", (_, pack) =>
-    socket.send(JSON.stringify({ command: "downloadModpack", modpackId: pack.id }))
-  )
   .on("downloadMod", (_, mod) =>
     socket.send(JSON.stringify({ command: "downloadMod", modId: mod.id, fileId: mod.fileId }))
   );
+
+ipcMain.on("downloadPack", (_, pack) => {
+  const eula = new BrowserWindow({
+    width: 935,
+    height: 595,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      devTools: false,
+    },
+  });
+
+  eula.loadFile("views/eula.html");
+
+  socket.send(JSON.stringify({ command: "downloadModpack", modpackId: pack.id }));
+});
 
 ipcMain.on("request", async (requester, data) => {
   const classId = data.class;
@@ -108,7 +131,7 @@ ipcMain.on("request", async (requester, data) => {
   param.set("searchFilter", data.name);
   param.set("sortField", "2");
   param.set("sortOrder", "desc");
-  if (data.class == "6" && version) {
+  if (classId == "6" && version) {
     param.set("gameVersion", version.split(" ")[1]);
     param.set("modLoaderType", loaderType[version.split(" ")[0]]);
   }
@@ -124,12 +147,16 @@ ipcMain.on("request", async (requester, data) => {
   if (req.ok) {
     const data = (await req.json()).data;
 
-    requester.reply("response", { class: classId, version: version.split(" ")[0], ...data });
+    if (classId == "6") {
+      requester.reply("response", { class: classId, version: version.split(" ")[0], ...data });
+    } else requester.reply("response", data);
+  } else {
+    console.log(`!req.ok: ${req.status}`);
   }
 });
 
 function getURL() {
-  return main.webContents.getURL().split("/").slice(-1)[0].split(".")[0];
+  return main?.webContents.getURL().split("/").slice(-1)[0].split(".")[0];
 }
 
 app.whenReady().then(() => {
